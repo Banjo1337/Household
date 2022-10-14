59cbca447a7619a0fdbe1d306769b68040fd1fae
tucoelgrande <bjorndjoefsson@gmail.com>
2022-10-14 14:48:23 +0200

    signup first draft

---
+++ components/CustomInput.tsx
@@ -0,0 +1,70 @@
+import React from "react";
+import { Control, Controller, RegisterOptions } from "react-hook-form";
+import { KeyboardTypeOptions, StyleSheet, Text, View } from "react-native";
+import { TextInput } from "react-native-paper";
+
+interface Props {
+    control: Control;
+    name: string;
+    rules?: Partial<RegisterOptions>;
+    placeholder: string;
+    secureTextEntry?: boolean;
+    maxLength?: number;
+    keyboardType?: KeyboardTypeOptions;
+    defaultValue?: string;
+}
+const CustomInput = ({
+    control,
+    name,
+    rules = {},
+    placeholder,
+    secureTextEntry,
+    maxLength,
+    keyboardType,
+    defaultValue = "",
+}: Props) => {
+    return (
+        <Controller
+            control={control}
+            name={name}
+            rules={rules}
+            render={({ field: { value, onChange, onBlur }, fieldState: { error } }) => (
+                <>
+                    <View style={[styles.container, { borderColor: error ? "red" : "#e8e8e8" }]}>
+                        <TextInput
+                            defaultValue={defaultValue}
+                            value={value}
+                            onChangeText={onChange}
+                            onBlur={onBlur}
+                            maxLength={maxLength}
+                            keyboardType={keyboardType}
+                            placeholder={placeholder}
+                            style={styles.input}
+                            secureTextEntry={secureTextEntry}
+                        />
+                    </View>
+                    {error && <Text style={{ color: "red", alignSelf: "stretch" }}>{error.message}</Text>}
+                </>
+            )}
+        />
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: "white",
+        justifyContent: "center",
+
+        minWidth: "100%",
+        width: "100%",
+
+        borderColor: "#e8e8e8",
+        borderWidth: 1,
+
+        borderRadius: 5,
+        marginVertical: 5,
+    },
+    input: {},
+});
+
+export default CustomInput;
--- screens/SignUpScreen.tsx
+++ screens/SignUpScreen.tsx
@@ -1,36 +1,96 @@
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
-import { View } from "react-native";
-import { Title } from "react-native-paper";
+import React from "react";
+import { useForm } from "react-hook-form";
+import { Pressable, ScrollView, StyleSheet, Text, View } from "react-native";
+import CustomInput from "../components/CustomInput";
 import { RootStackParamList } from "../NavContainer";
-import { SignUpReply } from "../features/authentication/authenticationTypes";
-import { useAppDispatch } from "../hooks/reduxHooks";
-import { useState } from "react";
 
 //Skall nog inte användas. Skall tjuvkolla på reactnative forms i puppy appen. :)
-type SignUpFormType = {
-  [fieldName: string]: string;
-};
+// type SignUpFormType = {
+//   [fieldName: string]: string;
+// };
 
-interface SignUpFormData {
-  username: string;
-  password: string;
-  passwordConfirmation: string;
-}
+// interface SignUpFormData {
+//   username: string;
+//   password: string;
+//   passwordConfirmation: string;
+// }
 
 export default function SignUpScreen(Props: NativeStackScreenProps<RootStackParamList, "SignUp">) {
-  const [fields, setFields] = useState<SignUpFormData>({
-    username: "",
-    password: "",
-    passwordConfirmation: "",
-  });
-  const dispatch = useAppDispatch();
+  // const [fields, setFields] = useState<SignUpFormData>({
+  //   username: "",
+  //   password: "",
+  //   passwordConfirmation: "",
+  // });
+  const { control, handleSubmit, watch } = useForm({});
+  const pwd = watch("password");
+
+  const onRegisterPressed = (data: any) => {
+    alert(data.username + data.password)
+  };
+  const onRegisterFailed = () => {
+    alert("Failed")
+  };
+
+
+  // const dispatch = useAppDispatch();
 
   //on submit something something...med RTK query
   //const reply: SignUpReplyType = await signUpHttpRequestAsync(dispatch, { username, email, password });
 
   return (
-    <View>
-      <Title>Hello from SignUpScreen</Title>
-    </View>
+    <ScrollView showsVerticalScrollIndicator={false}>
+      <View style={styles.container}>
+        <Text style={styles.title}>Create an account</Text>
+        <CustomInput
+          placeholder='Username'
+          name='username'
+          control={control}
+          rules={{ required: "Username is required" }}
+        />
+        <CustomInput
+          placeholder='Password'
+          name='password'
+          control={control}
+          secureTextEntry
+        />
+        <CustomInput
+          placeholder='Repeat password'
+          name='passwordRepeat'
+          control={control}
+          secureTextEntry
+          rules={{
+            required: "Repeat password",
+            validate: (value: any) => value === pwd || "Password not matching",
+          }}
+        />
+        <Pressable style={styles.pressable} onPress={handleSubmit(onRegisterPressed, onRegisterFailed)}>
+          <Text>Register</Text>
+        </Pressable>
+      </View>
+    </ScrollView>
   );
 }
+const styles = StyleSheet.create({
+  container: {
+    alignItems: "center",
+    padding: 20,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    color: "black",
+    backgroundColor: "gray",
+    margin: 10,
+  },
+  pressable: {
+    fontSize: 50,
+    fontWeight: "bold",
+    width: 100,
+    height: 30,
+    backgroundColor: "gray",
+    borderRadius: 5,
+    margin: 2,
+    borderColor: "black"
+  }
+});
