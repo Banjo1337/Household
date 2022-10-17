f2f1488931d7f45f3175459a54cd50f8067af86a
AlannAndre <80alaand@skola.boras.se>
2022-10-17 14:59:16 +0200

    add household reducer and other reducer

--- app/store.ts
+++ app/store.ts
@@ -1,9 +1,15 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { authenticateUserReducer } from "../features/authentication/authenticationSlice";
+import { householdReducer } from "../features/household/householdSlice";
 
 export const store = configureStore({
   reducer: {
     authenticateUserReducer: authenticateUserReducer,
+    householdReducer: householdReducer,
+    //pauseReducer: pauseReducer,
+    //profileRecuder: profileReducer,
+    //choreReducer: choreReducer,
+    //choreCompleted: choreCompleted,
   },
 });
 
