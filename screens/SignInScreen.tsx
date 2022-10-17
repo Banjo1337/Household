2710131043c8690205de86fa2fe14ce51f2e70f0
TucoElGrande <bjorndjosefsson@gmail.com>
2022-10-17 15:47:23 +0200

    template and navigation, typed value

--- screens/SignInScreen.tsx
+++ screens/SignInScreen.tsx
@@ -1,15 +1,15 @@
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import React from "react";
-import { useForm } from "react-hook-form";
+import { FieldValue, FieldValues, useForm } from "react-hook-form";
 import { Pressable, ScrollView, StyleSheet, Text, View } from "react-native";
 import CustomInput from "../components/CustomInput";
 import { useTogglePasswordVisibility } from "../hooks/useTogglePasswordVisibility";
 import { RootStackParamList } from "../NavContainer";
 
-export default function SignInScreen(
-  Props: NativeStackScreenProps<RootStackParamList, "SignIn">
-) {
+export default function SignInScreen({
+  navigation,
+}: NativeStackScreenProps<RootStackParamList>) {
   const { passwordVisibility, rightIcon, handlePasswordVisibility } =
     useTogglePasswordVisibility();
   const {
@@ -17,8 +17,9 @@ export default function SignInScreen(
     handleSubmit,
     formState: {},
   } = useForm();
-  const onLoginPressed = (data: any) => {
-    alert(data.username + data.password);
+  const onLoginPressed = (data: FieldValues) => {
+    console.log(data.username + data.password);
+    navigation.navigate("SelectProfile");
   };
   return (
     <ScrollView showsVerticalScrollIndicator={false}>
@@ -26,8 +27,15 @@ export default function SignInScreen(
         <Text style={styles.title}>Create an account</Text>
         <CustomInput placeholder="Username" name="username" control={control} />
         <View>
+          <Pressable onPress={handlePasswordVisibility}>
+            <MaterialCommunityIcons
+              name={rightIcon}
+              size={22}
+              color="#232323"
+            />
+          </Pressable>
           <CustomInput
-            placeholder="Username"
+            placeholder="Password"
             name="password"
             control={control}
             secureTextEntry={passwordVisibility}
@@ -37,13 +45,6 @@ export default function SignInScreen(
             onPress={handleSubmit(onLoginPressed)}
           >
             <Text>Sign in</Text>
-          </Pressable>
-          <Pressable onPress={handlePasswordVisibility}>
-            <MaterialCommunityIcons
-              name={rightIcon}
-              size={22}
-              color="#232323"
-            />
           </Pressable>
         </View>
       </View>
