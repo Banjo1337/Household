9d0bc27d8560de8a79d2e8a9dc94efad3d083c8f
AlannAndre <80alaand@skola.boras.se>
2022-10-17 14:59:39 +0200

    add dependencies

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
 import NavContainer from "./NavContainer";
 import { store } from "./app/store";
 import { Provider } from "react-redux";
+import React from "react";
 
 export default function App() {
   return (
