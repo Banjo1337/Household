cebf5e5765c06f86d4a6ca41bcd9092746a87eda
AlannAndre <80alaand@skola.boras.se>
2022-10-17 14:58:53 +0200

    add household types

---
+++ features/household/householdTypes.ts
@@ -0,0 +1,16 @@
+/* export type AuthenticationCredentials = {
+  username: string;
+  password: string;
+};
+
+export type Token = {
+  authUserId: string; //PK connection to AuthUserId (FK i Profile)
+  token: string;
+  expiration: Date; //not used right now. Token is set to be valid for 21 days in backend
+  //Format from backend is 2022-09-14T00:35:18Z
+};
+
+export type SignUpReply = {
+  status: string;
+  message: string;
+}; */
