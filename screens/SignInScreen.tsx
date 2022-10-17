b9d8e5122aa96071d87889118ea21f4eb6e77f82
TucoElGrande <bjorndjosefsson@gmail.com>
2022-10-17 10:19:51 +0200

    move toggle to hooks, fix some stuff input

--- screens/SignInScreen.tsx
+++ screens/SignInScreen.tsx
@@ -1,77 +1,75 @@
-import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import React from "react";
 import { useForm } from "react-hook-form";
 import { Pressable, ScrollView, StyleSheet, Text, View } from "react-native";
 import CustomInput from "../components/CustomInput";
-import { useTogglePasswordVisibility } from "../components/useTogglePasswordVisibility";
+import { useTogglePasswordVisibility } from "../hooks/useTogglePasswordVisibility";
 import { RootStackParamList } from "../NavContainer";
 
-
-export default function SignInScreen(Props: NativeStackScreenProps<RootStackParamList, "SignIn">) {
-
-    const { passwordVisibility, rightIcon, handlePasswordVisibility } =
-        useTogglePasswordVisibility();
-    const {
-        control,
-        handleSubmit,
-        formState: { },
-    } = useForm();
-    const onLoginPressed = (data: any) => {
-        alert(data)
-    };
-    return (
-        <ScrollView showsVerticalScrollIndicator={false}>
-            <View style={styles.container}>
-                <Text style={styles.title}>Create an account</Text>
-                <CustomInput
-                    placeholder='Username'
-                    defaultValue='banjo'
-                    value='banjo'
-                    name='username'
-                    control={control}
-                />
-                <View>
-                    <CustomInput
-                        placeholder='Username'
-                        defaultValue='banjo'
-                        value='banjo'
-                        name='password'
-                        control={control}
-                    />
-                    <Pressable style={styles.pressable} onPress={handleSubmit(onLoginPressed)}>
-                        <Text>Sign in</Text>
-                    </Pressable>
-                    <Pressable onPress={handlePasswordVisibility}>
-                        {/* Why is this wrong (name={rightIcon})??? */}
-                        <MaterialCommunityIcons name={rightIcon} size={22} color="#232323" />
-                    </Pressable>
-                </View>
-
-            </View>
-        </ScrollView>
-    )
+export default function SignInScreen(
+  Props: NativeStackScreenProps<RootStackParamList, "SignIn">
+) {
+  const { passwordVisibility, rightIcon, handlePasswordVisibility } =
+    useTogglePasswordVisibility();
+  const {
+    control,
+    handleSubmit,
+    formState: {},
+  } = useForm();
+  const onLoginPressed = (data: any) => {
+    alert(data.username + data.password);
+  };
+  return (
+    <ScrollView showsVerticalScrollIndicator={false}>
+      <View style={styles.container}>
+        <Text style={styles.title}>Create an account</Text>
+        <CustomInput placeholder="Username" name="username" control={control} />
+        <View>
+          <CustomInput
+            placeholder="Username"
+            name="password"
+            control={control}
+            secureTextEntry={passwordVisibility}
+          />
+          <Pressable
+            style={styles.pressable}
+            onPress={handleSubmit(onLoginPressed)}
+          >
+            <Text>Sign in</Text>
+          </Pressable>
+          <Pressable onPress={handlePasswordVisibility}>
+            <MaterialCommunityIcons
+              name={rightIcon}
+              size={22}
+              color="#232323"
+            />
+          </Pressable>
+        </View>
+      </View>
+    </ScrollView>
+  );
 }
 const styles = StyleSheet.create({
-    container: {
-        alignItems: "center",
-        padding: 20,
-    },
-    title: {
-        fontSize: 24,
-        fontWeight: "bold",
-        color: "black",
-        backgroundColor: "gray",
-        margin: 10,
-    },
-    pressable: {
-        fontSize: 50,
-        fontWeight: "bold",
-        width: 100,
-        height: 30,
-        backgroundColor: "gray",
-        borderRadius: 5,
-        margin: 2,
-        borderColor: "black"
-    }
+  container: {
+    alignItems: "center",
+    padding: 20,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    color: "black",
+    backgroundColor: "gray",
+    margin: 10,
+  },
+  pressable: {
+    fontSize: 50,
+    fontWeight: "bold",
+    width: 100,
+    height: 30,
+    backgroundColor: "gray",
+    borderRadius: 5,
+    margin: 2,
+    borderColor: "black",
+  },
 });
--- screens/SignUpScreen.tsx
+++ screens/SignUpScreen.tsx
@@ -1,40 +1,27 @@
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import React from "react";
 import { useForm } from "react-hook-form";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 import { Pressable, ScrollView, StyleSheet, Text, View } from "react-native";
 import CustomInput from "../components/CustomInput";
+import { useTogglePasswordVisibility } from "../hooks/useTogglePasswordVisibility";
 import { RootStackParamList } from "../NavContainer";
 
-//Skall nog inte användas. Skall tjuvkolla på reactnative forms i puppy appen. :)
-// type SignUpFormType = {
-//   [fieldName: string]: string;
-// };
-
-// interface SignUpFormData {
-//   username: string;
-//   password: string;
-//   passwordConfirmation: string;
-// }
-
-export default function SignUpScreen(Props: NativeStackScreenProps<RootStackParamList, "SignUp">) {
-  // const [fields, setFields] = useState<SignUpFormData>({
-  //   username: "",
-  //   password: "",
-  //   passwordConfirmation: "",
-  // });
+export default function SignUpScreen(
+  Props: NativeStackScreenProps<RootStackParamList, "SignUp">
+) {
+  const { passwordVisibility, rightIcon, handlePasswordVisibility } =
+    useTogglePasswordVisibility();
   const { control, handleSubmit, watch } = useForm({});
   const pwd = watch("password");
-
   const onRegisterPressed = (data: any) => {
-    alert(data.username + data.password)
+    alert(data.username + data.password);
   };
   const onRegisterFailed = () => {
-    alert("Failed")
+    alert("Failed");
   };
 
-
   // const dispatch = useAppDispatch();
-
   //on submit something something...med RTK query
   //const reply: SignUpReplyType = await signUpHttpRequestAsync(dispatch, { username, email, password });
 
@@ -43,20 +30,20 @@ export default function SignUpScreen(Props: NativeStackScreenProps<RootStackPara
       <View style={styles.container}>
         <Text style={styles.title}>Create an account</Text>
         <CustomInput
-          placeholder='Username'
-          name='username'
+          placeholder="Username"
+          name="username"
           control={control}
           rules={{ required: "Username is required" }}
         />
         <CustomInput
-          placeholder='Password'
-          name='password'
+          placeholder="Password"
+          name="password"
           control={control}
           secureTextEntry
         />
         <CustomInput
-          placeholder='Repeat password'
-          name='passwordRepeat'
+          placeholder="Repeat password"
+          name="passwordRepeat"
           control={control}
           secureTextEntry
           rules={{
@@ -64,8 +51,18 @@ export default function SignUpScreen(Props: NativeStackScreenProps<RootStackPara
             validate: (value: any) => value === pwd || "Password not matching",
           }}
         />
-        <Pressable style={styles.pressable} onPress={handleSubmit(onRegisterPressed, onRegisterFailed)}>
+        <Pressable
+          style={styles.pressable}
+          onPress={handleSubmit(onRegisterPressed, onRegisterFailed)}
+        >
           <Text>Register</Text>
+          <Pressable onPress={handlePasswordVisibility}>
+            <MaterialCommunityIcons
+              name={rightIcon}
+              size={22}
+              color="#232323"
+            />
+          </Pressable>
         </Pressable>
       </View>
     </ScrollView>
@@ -91,6 +88,6 @@ const styles = StyleSheet.create({
     backgroundColor: "gray",
     borderRadius: 5,
     margin: 2,
-    borderColor: "black"
-  }
+    borderColor: "black",
+  },
 });
