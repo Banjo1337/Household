e4190176227e54fc8c844d325374f07d3a92ce8d
Robin Keturi <Robin.Keturi@gmail.com>
2022-10-19 21:24:56 +0200

    formatting

--- features/theme/ThemeContext.tsx
+++ features/theme/ThemeContext.tsx
@@ -5,12 +5,12 @@ import useSecureStorage from "../../hooks/useSecureStorage";
 import { DarkTheme, LightTheme, Theme } from "./themeTypes";
 
 interface ContextValue {
-    darkmode: boolean;
-    setDarkmode: Dispatch<React.SetStateAction<boolean>>
-    systemTheme: boolean;
-    setSystemTheme: Dispatch<React.SetStateAction<boolean>>
-    currentTheme: Theme;
-    setCurrentTheme: Dispatch<React.SetStateAction<Theme>>
+  darkmode: boolean;
+  setDarkmode: Dispatch<React.SetStateAction<boolean>>;
+  systemTheme: boolean;
+  setSystemTheme: Dispatch<React.SetStateAction<boolean>>;
+  currentTheme: Theme;
+  setCurrentTheme: Dispatch<React.SetStateAction<Theme>>;
 }
 
 const ThemeContext = createContext<ContextValue>({} as ContextValue);
@@ -20,26 +20,20 @@ interface Props {
 }
 
 export default function ThemeProvider({ children }: Props) {
-    const [darkmode, setDarkmode] = useSecureStorage<boolean>("darkmode", false);
-    const [systemTheme, setSystemTheme] = useSecureStorage<boolean>("autoTheme", true);
-    const [currentTheme, setCurrentTheme] = useState<Theme>(LightTheme);
-    
-    useEffect(() => {
-        if(systemTheme){
-            const systemTheme = Appearance.getColorScheme();
-            setCurrentTheme(systemTheme === "dark" ? DarkTheme : LightTheme);
-        } else {
-            setCurrentTheme(darkmode ? DarkTheme : LightTheme);
-        }
-    }, [systemTheme, darkmode]);
+  const [darkmode, setDarkmode] = useSecureStorage<boolean>("darkmode", false);
+  const [systemTheme, setSystemTheme] = useSecureStorage<boolean>("autoTheme", true);
+  const [currentTheme, setCurrentTheme] = useState<Theme>(LightTheme);
 
-    return (
-        <ThemeContext.Provider
-        value={{ darkmode, setDarkmode, systemTheme, setSystemTheme, currentTheme, setCurrentTheme }}
-        >
-        {children}
-        </ThemeContext.Provider>
-    );
+  useEffect(() => {
+    if (systemTheme) {
+      const systemTheme = Appearance.getColorScheme();
+      setCurrentTheme(systemTheme === "dark" ? DarkTheme : LightTheme);
+    } else {
+      setCurrentTheme(darkmode ? DarkTheme : LightTheme);
+    }
+  }, [systemTheme, darkmode]);
+
+  return <ThemeContext.Provider value={{ darkmode, setDarkmode, systemTheme, setSystemTheme, currentTheme, setCurrentTheme }}>{children}</ThemeContext.Provider>;
 }
 
 export function useTheme() {
