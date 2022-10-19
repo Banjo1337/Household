10ea3598373475abe980ae4e272eb215bdf573b7
AlannAndre <80alaand@skola.boras.se>
2022-10-19 15:35:26 +0200

    Add mock-up data for testing

--- screens/EditHouseholdScreen.tsx
+++ screens/EditHouseholdScreen.tsx
@@ -1,8 +1,16 @@
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import React, { useReducer, useState } from "react";
 import { Field, FieldValue, FieldValues, useForm } from "react-hook-form";
-import { Pressable, View, Text, StyleSheet } from "react-native";
-import { Title, TextInput } from "react-native-paper";
+import {
+  Pressable,
+  View,
+  Text,
+  StyleSheet,
+  Image,
+  TouchableOpacity,
+} from "react-native";
+import { Title, TextInput, Switch } from "react-native-paper";
+import { shouldUseActivityState } from "react-native-screens";
 import CustomInput from "../components/CustomInput";
 import { RootStackParamList } from "../NavContainer";
 
@@ -13,27 +21,61 @@ export default function EditHouseholdScreen({ navigation }: NativeStackScreenPro
     //formState: {},
   } = useForm();
 
+//Mock-up data
+const household = {
+  householdName: "My household",
+  householdPicture: "",
+  householdCode: "aE7fZ",
+  members: ["AA","BB","CC"]
+}
+
   const onEditHouseholdPressed = (data: FieldValues) => {
     //TODO: add dispatch from EditHouseholdSlice
     console.log("you have pressed edit household" + data.householdName);  
     navigation.navigate("MegaNavigationGod");
   };
 
+  var householdPicture = "../assets/household.png";
+
   return (
     <View style={styles.container}>
       <View>
-        <TextInput>Current Household name: XXXXX</TextInput>
+        <Image
+          source={require(householdPicture)}
+          style={styles.householdPicture}
+        />
+        <Text>Household's name: </Text>
+        <Text style={styles.showProperty}>{household.householdName}</Text>
+
+        <Text>Change household's name: </Text>
         <CustomInput
           name="householdName"
           placeholder="Enter a new household name"
           control={control}
         ></CustomInput>
-        <Pressable
+        <TouchableOpacity
           style={styles.pressable}
           onPress={handleSubmit(onEditHouseholdPressed)}
         >
-          <Text>Create Household</Text>
-        </Pressable>
+          <Text>Submit</Text>
+        </TouchableOpacity>
+        <Text>Household code: </Text>
+        <Text style={styles.showProperty}>{household.householdCode}</Text>
+        <Text>Household members: </Text>
+        <View style={{ flex: 1, flexDirection: "row" }}>
+          {household.members.map((member, memberIndex) => {
+            return (
+              <View style={{ padding: 5 }} key={memberIndex}>
+                <Image
+                  source={require(householdPicture)}
+                  style={styles.householdMemberPicture}
+                />
+                <Text style={{ textAlign: "center" }}>{member}</Text>
+                <Switch></Switch>
+              </View>
+            );
+          })}
+        </View>
       </View>
     </View>
   );
@@ -53,5 +95,23 @@ const styles = StyleSheet.create({
     borderRadius: 5,
     margin: 2,
     borderColor: "red",
+  },
+  showProperty: {
+    alignItems: "center",
+    fontSize: 18,
+    borderWidth: 1,
+    borderRadius: 10,
+    padding: 10,
+    borderColor: "black",
+  },
+  householdPicture: {
+    width: 120,
+    height: 120,
+    borderRadius: 20,
+  },
+  householdMemberPicture: {
+    width: 50,
+    height: 50,
+    borderRadius: 20,
   },
 });
