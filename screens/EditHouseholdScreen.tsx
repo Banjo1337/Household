8a4a2e3af6324fc2c35724d6c4d28623461187e8
AlannAndre <80alaand@skola.boras.se>
2022-10-24 23:31:49 +0200

    add avatar but need to fix eslint-desibla-next-line.....

--- screens/EditHouseholdScreen.tsx
+++ screens/EditHouseholdScreen.tsx
@@ -1,33 +1,27 @@
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import React from "react";
 import { FieldValues, useForm } from "react-hook-form";
-import {
-  View,
-  Text,
-  StyleSheet,
-  Image,
-  TouchableOpacity,
-  ScrollView,
-} from "react-native";
+import { View, Text, StyleSheet, Image, TouchableOpacity, ScrollView } from "react-native";
 import { Switch } from "react-native-paper";
+import { white } from "react-native-paper/lib/typescript/styles/themes/v2/colors";
 
 import CustomInput from "../components/CustomInput";
-import { selectHousehold, selectProfileByHousholdId } from "../features/household/householdSelectors";
 import {
-  editHouseholdThunk,
-} from "../features/household/householdSlice";
+  selectHousehold,
+  selectProfileByHousholdId,
+} from "../features/household/householdSelectors";
+import { editHouseholdThunk } from "../features/household/householdSlice";
 import { useAppDispatch, useAppSelector } from "../hooks/reduxHooks";
+import { useAvatar } from "../hooks/useAvatar";
 import { RootStackParamList } from "../NavContainer";
 
-export default function EditHouseholdScreen({
-  route,
-}: NativeStackScreenProps<RootStackParamList>) {
+export default function EditHouseholdScreen({ route }: NativeStackScreenProps<RootStackParamList>) {
   //add route.params for householdId instead of "stringPlaceholder";
   const householdId = "stringPlaceholder";
   const dispatch = useAppDispatch();
   const household = useAppSelector(selectHousehold);
   const members = useAppSelector(selectProfileByHousholdId);
-
+  
   const {
     control,
     handleSubmit,
@@ -40,7 +34,7 @@ export default function EditHouseholdScreen({
       editHouseholdThunk({
         householdEditDto: { name: name },
         householdId: householdId,
-      })
+      }),
     );
   };
 
@@ -50,51 +44,53 @@ export default function EditHouseholdScreen({
     <ScrollView>
       <View style={styles.container}>
         <View>
-          <Image
-            source={require(householdPicture)}
-            style={styles.householdPicture}
-          />
+          <Image source={require(householdPicture)} style={styles.householdPicture} />
           <Text>Household's name: </Text>
           <Text style={styles.showProperty}>{household.name}</Text>
           <Text>Household's admin: </Text>
           <View style={{ flex: 1, flexDirection: "row" }}>
             {members.map((member, memberIndex) => {
+              
               if (member.isAdmin) {
+                // eslint-disable-next-line react-hooks/rules-of-hooks
+                const { emoji, color } = useAvatar(member.avatar);
                 return (
                   <View style={{ padding: 5 }} key={memberIndex}>
-                    <Text style={{ textAlign: "center" }}>{member.avatar}</Text>
-                    <Text style={{ textAlign: "center" }}>{member.alias}</Text>
-                    <Switch></Switch>
+                    <Text style={styles.avatar}>{emoji}</Text>
+                    <Text style={{ textAlign: "center", backgroundColor: color}}>
+                      {member.alias}
+                    </Text>
                   </View>
                 );
               }
             })}
           </View>
 
-{/* Conditional rendering needed here to render only i user is admin */}
-          <Text>Change household's name: </Text> 
+          {/* Conditional rendering needed here to render only i user is admin */}
+          <Text>Change household's name: </Text>
           <CustomInput
-            name="householdName"
-            placeholder="Enter a new household name"
+            name='householdName'
+            placeholder='Enter a new household name'
             control={control}
           ></CustomInput>
-          <TouchableOpacity
-            style={styles.pressable}
-            onPress={handleSubmit(onEditHouseholdPressed)}
-          >
+          <TouchableOpacity style={styles.pressable} onPress={handleSubmit(onEditHouseholdPressed)}>
             <Text>Submit</Text>
           </TouchableOpacity>
-          
-          
+
           <Text>Household code: </Text>
           <Text style={styles.showProperty}>{household.code}</Text>
           <Text>Household members: </Text>
           <View style={{ flex: 1, flexDirection: "row" }}>
             {members.map((member, memberIndex) => {
+              
+              // eslint-disable-next-line react-hooks/rules-of-hooks
+              const { emoji, color } = useAvatar(member.avatar);
               return (
                 <View style={{ padding: 5 }} key={memberIndex}>
-                  <Text style={{ textAlign: "center" }}>{member.avatar}</Text>
-                  <Text style={{ textAlign: "center" }}>{member.alias}</Text>
+                  <Text style={styles.avatar}>{emoji}</Text>
+                  <Text style={{ textAlign: "center", backgroundColor: color }}>
+                    {member.alias}
+                  </Text>
                   <Switch></Switch>
                 </View>
               );
@@ -139,5 +135,9 @@ const styles = StyleSheet.create({
     height: 50,
     margin: 0,
     borderRadius: 20,
+  },
+  avatar: {
+    fontSize: 50,
+    textAlign: "center",
   },
 });
