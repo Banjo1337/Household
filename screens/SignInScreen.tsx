2a807a2f566128de7d215258031e6c5adac9b228
CmdrMaylee <meleechan@gmail.com>
2022-10-24 13:20:44 +0200

    Styled SignupScreen Adjusted password eye color Added image to Signin Set both screens in a ScrollView to allow the screen moving when opening the Keyboard in front of an input field

--- screens/SignInScreen.tsx
+++ screens/SignInScreen.tsx
@@ -3,9 +3,8 @@ import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import * as SecureStore from "expo-secure-store";
 import React, { useEffect, useState } from "react";
 import { FieldValues, useForm } from "react-hook-form";
-import { StyleSheet, View } from "react-native";
+import { Image, Pressable, ScrollView, StyleSheet, View } from "react-native";
 import { Button } from "react-native-paper";
-import { useTheme } from "../features/theme/ThemeContext";
 import CustomInput from "../components/CustomInput";
 import {
   logout,
@@ -14,6 +13,7 @@ import {
   selectErrorText,
   selectHasError,
 } from "../features/authentication/authenticationSlice";
+import { useTheme } from "../features/theme/ThemeContext";
 import { useAppDispatch, useAppSelector } from "../hooks/reduxHooks";
 import { useTogglePasswordVisibility } from "../hooks/useTogglePasswordVisibility";
 import { RootStackParamList } from "../NavContainer";
@@ -61,48 +61,56 @@ export default function SignInScreen({ navigation }: NativeStackScreenProps<Root
   };
 
   return (
-    <View style={styles.container}>
-      <CustomInput
-        placeholder='Username'
-        name='username'
-        control={control}
-        rules={{
-          required: "Username is required",
-          minLength: { value: 4, message: "Must be minimum 4 " },
-          maxLength: { value: 50, message: "Cant be more than 50 letters" },
-        }}
-      />
-
-      <MaterialCommunityIcons
-        name={rightIcon}
-        size={32}
-        color={currentTheme.dark ? "#232323" : "#333"}
-        onPress={handlePasswordVisibility}
-        style={[styles.passwordPosition, { zIndex: 1 }]}
-        // Places the icon between the two inputs, and draws it on top of the below CustomInput because of zIndex.
-      />
-      <CustomInput
-        placeholder='Password'
-        name='password'
-        control={control}
-        secureTextEntry={passwordVisibility}
-        rules={{ required: "Password is required" }}
-      />
-      <Button style={styles.button} onPress={handleSubmit(onLoginPressed)}>
-        Sign In
-      </Button>
-      <Button style={styles.button} onPress={onLogoutPressed}>
-        Logout
-      </Button>
-    </View>
+    <ScrollView>
+      <View style={styles.container}>
+        <Image style={styles.image} source={require("../assets/household.png")} />
+        <CustomInput
+          placeholder='Username'
+          name='username'
+          control={control}
+          rules={{
+            required: "Username is required",
+            minLength: { value: 4, message: "Must be minimum 4 " },
+            maxLength: { value: 50, message: "Cant be more than 50 letters" },
+          }}
+        />
+        <Pressable onPress={handlePasswordVisibility} style={styles.eye}>
+          <MaterialCommunityIcons
+            name={rightIcon}
+            size={32}
+            color={currentTheme.dark ? "#dedede" : "#232323"}
+            // Places the icon between the two inputs, and draws it on top of the below CustomInput because of zIndex.
+          />
+        </Pressable>
+        <CustomInput
+          placeholder='Password'
+          name='password'
+          control={control}
+          secureTextEntry={passwordVisibility}
+          rules={{ required: "Password is required" }}
+        />
+        <Button style={styles.button} onPress={handleSubmit(onLoginPressed)}>
+          Sign In
+        </Button>
+        <Button style={styles.button} onPress={onLogoutPressed}>
+          Logout
+        </Button>
+      </View>
+    </ScrollView>
   );
 }
 const styles = StyleSheet.create({
   container: {
+    flex: 1,
     alignItems: "center",
     paddingHorizontal: 20,
-    marginTop: 100,
+  },
+  image: {
+    height: 225,
+    width: 225,
+    borderRadius: 40,
+    marginVertical: 30,
   },
   button: { marginTop: 50 },
-  passwordPosition: { position: "relative", left: 150, top: 45 },
+  eye: { position: "relative", left: 150, top: 45, zIndex: 1 },
 });
--- screens/SignUpScreen.tsx
+++ screens/SignUpScreen.tsx
@@ -1,15 +1,15 @@
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import React from "react";
 import { FieldValues, useForm } from "react-hook-form";
-import { MaterialCommunityIcons } from "@expo/vector-icons";
-import { Pressable, ScrollView, StyleSheet, Text, View } from "react-native";
+import { Image, Pressable, ScrollView, StyleSheet, View } from "react-native";
+import { Button } from "react-native-paper";
 import CustomInput from "../components/CustomInput";
+import { useTheme } from "../features/theme/ThemeContext";
 import { useTogglePasswordVisibility } from "../hooks/useTogglePasswordVisibility";
 import { RootStackParamList } from "../NavContainer";
-import { Button, Title } from "react-native-paper";
 
-const PWD_REGEX =
-  /^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[*.!@$%^&(){}[]:;<>,.?~_+-=|).{6,32}$/;
+const PWD_REGEX = /^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[*.!@$%^&(){}[]:;<>,.?~_+-=|).{6,32}$/;
 
 interface SignUpDto {
   username: string;
@@ -22,14 +22,10 @@ type SignUpResponse = {
   message: string;
 };
 
-const BASE_URL =
-  "https://household-backend.azurewebsites.net/api/V01/Authenticate/";
+const BASE_URL = "https://household-backend.azurewebsites.net/api/V01/Authenticate/";
 
-export default function SignUpScreen({
-  navigation,
-}: NativeStackScreenProps<RootStackParamList>) {
-  const { passwordVisibility, rightIcon, handlePasswordVisibility } =
-    useTogglePasswordVisibility();
+export default function SignUpScreen({ navigation }: NativeStackScreenProps<RootStackParamList>) {
+  const { passwordVisibility, rightIcon, handlePasswordVisibility } = useTogglePasswordVisibility();
   const {
     control,
     handleSubmit,
@@ -37,6 +33,7 @@ export default function SignUpScreen({
     formState: {},
   } = useForm({});
   const pwd = watch("password");
+  const { currentTheme } = useTheme();
 
   const onRegisterPressed = (data: FieldValues) => {
     console.log(data.username + data.password);
@@ -62,9 +59,7 @@ export default function SignUpScreen({
       const response: Response = await PostSignUp(signUpDto);
       try {
         if (response.status != 200) {
-          throw new Error(
-            "Httprequest to get token failed, response is not 200"
-          );
+          throw new Error("Httprequest to get token failed, response is not 200");
         }
         const signUpResponse = (await response.json()) as SignUpResponse;
         return signUpResponse;
@@ -96,13 +91,12 @@ export default function SignUpScreen({
     }
   };
   return (
-    <ScrollView showsVerticalScrollIndicator={false}>
+    <ScrollView>
       <View style={styles.container}>
-        <Title style={styles.title}>Sign Up Screen</Title>
+        <Image style={styles.image} source={require("../assets/household.png")} />
         <CustomInput
-          style={styles.input}
-          placeholder="Username"
-          name="username"
+          placeholder='Username'
+          name='username'
           control={control}
           rules={{
             required: "Username is required",
@@ -110,14 +104,9 @@ export default function SignUpScreen({
             maxLength: { value: 50, message: "Cant be more than 50 letters" },
           }}
         />
-        <Text style={styles.title}>Toggle password visibility</Text>
-        <Pressable style={styles.pressable} onPress={handlePasswordVisibility}>
-          <MaterialCommunityIcons name={rightIcon} size={22} color="#232323" />
-        </Pressable>
         <CustomInput
-          style={styles.input}
-          placeholder="Password"
-          name="password"
+          placeholder='Password'
+          name='password'
           control={control}
           secureTextEntry={passwordVisibility}
           rules={{
@@ -133,22 +122,24 @@ export default function SignUpScreen({
             },
           }}
         />
+        <Pressable onPress={handlePasswordVisibility} style={styles.eye}>
+          <MaterialCommunityIcons
+            name={rightIcon}
+            size={32}
+            color={currentTheme.dark ? "#dedede" : "#232323"}
+          />
+        </Pressable>
         <CustomInput
-          style={styles.input}
-          placeholder="Repeat password"
-          name="passwordRepeat"
+          placeholder='Repeat password'
+          name='passwordRepeat'
           control={control}
           secureTextEntry={passwordVisibility}
           rules={{
             required: "Repeat password",
-            validate: (value: string) =>
-              value === pwd || "Password not matching",
+            validate: (value: string) => value === pwd || "Password not matching",
           }}
         />
-        <Button
-          style={styles.button}
-          onPress={handleSubmit(onRegisterPressed, onRegisterFailed)}
-        >
+        <Button style={styles.button} onPress={handleSubmit(onRegisterPressed, onRegisterFailed)}>
           Register
         </Button>
       </View>
@@ -157,15 +148,16 @@ export default function SignUpScreen({
 }
 const styles = StyleSheet.create({
   container: {
+    flex: 1,
     alignItems: "center",
-    backgroundColor: "green",
-    padding: 20,
+    paddingHorizontal: 20,
   },
-  title: {
-    backgroundColor: "white",
+  image: {
+    height: 225,
+    width: 225,
+    borderRadius: 40,
+    marginVertical: 30,
   },
-  button: { backgroundColor: "hotpink" },
-  pressable: { backgroundColor: "red" },
-  input: { backgroundColor: "brown" },
-  dropDownPicker: { backgroundColor: "yellow" },
+  eye: { position: "relative", left: 150, top: 45, zIndex: 1 },
+  button: { backgroundColor: "lightgrey", paddingHorizontal: 30, marginTop: 40 },
 });
