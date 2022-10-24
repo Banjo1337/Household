69011bb497fe73f2ea9d4767d9eb636c12f773f6
TucoElGrande <bjorndjosefsson@gmail.com>
2022-10-24 10:28:15 +0200

    choreslice bugfix

--- features/chore/choreSlice.ts
+++ features/chore/choreSlice.ts
@@ -13,36 +13,43 @@ async function getToken(): Promise<string> {
   }
 }
 
-export const createChore = createAsyncThunk<Chore, ChoreCreateDto, { rejectValue: string }>("chore/CreateChore", async (choreCreateDto: ChoreCreateDto, thunkApi) => {
-  const token = await getToken();
-  if (!token) {
-    return thunkApi.rejectWithValue("User not logged in");
-  }
-  try {
-    const response = await fetch(baseUrl + "AddChore", {
-      method: "POST",
-      headers: {
-        "content-type": "application/json",
-        authorization: "Bearer " + token,
-      },
-      body: JSON.stringify(choreCreateDto),
-    });
-
-    if (response.ok) {
-      return (await response.json()) as Chore;
+export const createChore = createAsyncThunk<Chore, ChoreCreateDto, { rejectValue: string }>(
+  "chore/CreateChore",
+  async (choreCreateDto: ChoreCreateDto, thunkApi) => {
+    const token = await getToken();
+    if (!token) {
+      return thunkApi.rejectWithValue("User not logged in");
     }
+    try {
+      const response = await fetch(baseUrl + "AddChore", {
+        method: "POST",
+        headers: {
+          "content-type": "application/json",
+          authorization: "Bearer " + token,
+        },
+        body: JSON.stringify(choreCreateDto),
+      });
 
-    return thunkApi.rejectWithValue(JSON.stringify(response.body));
-  } catch (err) {
-    if (err instanceof Error) {
-      return thunkApi.rejectWithValue(err.message);
-    } else {
-      return thunkApi.rejectWithValue("");
+      if (response.ok) {
+        return (await response.json()) as Chore;
+      }
+
+      return thunkApi.rejectWithValue(JSON.stringify(response.body));
+    } catch (err) {
+      if (err instanceof Error) {
+        return thunkApi.rejectWithValue(err.message);
+      } else {
+        return thunkApi.rejectWithValue("");
+      }
     }
-  }
-});
+  },
+);
 
-export const updateChore = createAsyncThunk<Chore, { choreUpdateDto: ChoreUpdateDto; choreId: string }, { rejectValue: string }>("chore/UpdateChore", async ({ choreUpdateDto, choreId }, thunkApi) => {
+export const updateChore = createAsyncThunk<
+  Chore,
+  { choreUpdateDto: ChoreUpdateDto; choreId: string },
+  { rejectValue: string }
+>("chore/UpdateChore", async ({ choreUpdateDto, choreId }, thunkApi) => {
   const token = await getToken();
   if (!token) {
     return thunkApi.rejectWithValue("User not logged in");
@@ -71,33 +78,36 @@ export const updateChore = createAsyncThunk<Chore, { choreUpdateDto: ChoreUpdate
   }
 });
 
-export const deleteChore = createAsyncThunk<Chore, string, { rejectValue: string }>("chore/deleteChore", async (choreId: string, thunkApi) => {
-  const token = await getToken();
-  if (!token) {
-    return thunkApi.rejectWithValue("User not logged in");
-  }
-  try {
-    const response = await fetch(baseUrl + "DeleteChore/" + choreId, {
-      method: "DELETE",
-      headers: {
-        "content-type": "application/json",
-        authorization: "Bearer " + token,
-      },
-    });
-
-    if (response.status == 204) {
-      return {} as Chore;
+export const deleteChore = createAsyncThunk<Chore, string, { rejectValue: string }>(
+  "chore/deleteChore",
+  async (choreId: string, thunkApi) => {
+    const token = await getToken();
+    if (!token) {
+      return thunkApi.rejectWithValue("User not logged in");
     }
+    try {
+      const response = await fetch(baseUrl + "DeleteChore/" + choreId, {
+        method: "DELETE",
+        headers: {
+          "content-type": "application/json",
+          authorization: "Bearer " + token,
+        },
+      });
 
-    return thunkApi.rejectWithValue(JSON.stringify(response.body));
-  } catch (err) {
-    if (err instanceof Error) {
-      return thunkApi.rejectWithValue(err.message);
-    } else {
-      return thunkApi.rejectWithValue("");
+      if (response.status == 204) {
+        return {} as Chore;
+      }
+
+      return thunkApi.rejectWithValue(JSON.stringify(response.body));
+    } catch (err) {
+      if (err instanceof Error) {
+        return thunkApi.rejectWithValue(err.message);
+      } else {
+        return thunkApi.rejectWithValue("");
+      }
     }
-  }
-});
+  },
+);
 
 const initialState: ChoreState = {
   chores: {} as Chore[],
@@ -111,31 +121,37 @@ const choreSlice = createSlice({
   reducers: {},
 
   extraReducers: (builder) => {
-    builder.addMatcher(isAnyOf(createChore.pending, updateChore.pending, deleteChore.pending), (state) => {
-      state.isLoading = true;
-      state.error = "";
-    });
-
     builder.addCase(createChore.fulfilled, (state, action: PayloadAction<Chore>) => {
       state.isLoading = false;
       state.chores.push(action.payload);
     });
     builder.addCase(updateChore.fulfilled, (state, action: PayloadAction<Chore>) => {
       state.isLoading = false;
-      state.chores = state.chores.map((chore) => (chore.id == action.payload.id ? action.payload : chore));
+      state.chores = state.chores.map((chore) =>
+        chore.id == action.payload.id ? action.payload : chore,
+      );
     });
     builder.addCase(deleteChore.fulfilled, (state, action: PayloadAction<Chore>) => {
       state.isLoading = false;
       state.chores = state.chores.filter((chore) => chore.id != action.payload.id);
     });
-
-    builder.addMatcher(isAnyOf(createChore.rejected, createChore.rejected, createChore.rejected), (state, action) => {
-      if (action.payload) {
-        state.error = action.payload;
-        console.log(action.payload);
-      }
-      state.isLoading = false;
-    });
+    builder.addMatcher(
+      isAnyOf(createChore.pending, updateChore.pending, deleteChore.pending),
+      (state) => {
+        state.isLoading = true;
+        state.error = "";
+      },
+    );
+    builder.addMatcher(
+      isAnyOf(createChore.rejected, createChore.rejected, createChore.rejected),
+      (state, action) => {
+        if (action.payload) {
+          state.error = action.payload;
+          console.log(action.payload);
+        }
+        state.isLoading = false;
+      },
+    );
   },
 });
 
