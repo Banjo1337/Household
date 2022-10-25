40ff1639968ee1eacaa82bc8d6b3693d75a85eac
CmdrMaylee <meleechan@gmail.com>
2022-10-25 16:01:22 +0200

    Styled CreateProfileScreen. (Button needs fixing when opening keyboard). Also fixed imports in SignInScreen.

--- screens/CreateProfileScreen.tsx
+++ screens/CreateProfileScreen.tsx
@@ -1,8 +1,8 @@
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import React, { useState } from "react";
 import { FieldValues, useForm } from "react-hook-form";
-import { View, Text, StyleSheet, ScrollView } from "react-native";
-import { Button, Title } from "react-native-paper";
+import { ScrollView, StyleSheet, Text, View } from "react-native";
+import { Button } from "react-native-paper";
 import CustomInput from "../components/CustomInput";
 import { RootStackParamList } from "../NavContainer";
 
@@ -15,33 +15,26 @@ export default function CreateProfileScreen({
     formState: {},
   } = useForm();
   const toggleInput = () => {
-    setInput((input) => !input);
-    console.log("toggle pressed");
+    setInput((joinHousehold) => !joinHousehold);
+    console.log("toggle pressed" + joinAsUser);
   };
-  const [input, setInput] = useState(false);
+  const [joinAsUser, setInput] = useState(false);
 
   const onCreateHouseholdPressed = (data: FieldValues) => {
-    console.log(
-      "you have pressed create household" +
-        data.householdname +
-        data.profilename
-    );
-    navigation.navigate("Home", {screen: "Chores"});
+    console.log("you have pressed create household" + data.householdname + data.profilename);
+    navigation.navigate("Home", { screen: "Chores" });
   };
   const onJoinHouseholdPressed = (data: FieldValues) => {
-    console.log(
-      "you have pressed join household" + data.householdname + data.profilename
-    );
+    console.log("you have pressed join household" + data.householdname + data.profilename);
     navigation.navigate("PendingRequest");
   };
   return (
-    <ScrollView showsVerticalScrollIndicator={false}>
+    <ScrollView showsVerticalScrollIndicator={false} contentContainerStyle={styles.scrollContainer}>
       <View style={styles.container}>
-        <Title style={styles.title}>Add Chore</Title>
         <CustomInput
           style={styles.input}
-          name="profilename"
-          placeholder="Desired profile name"
+          name='profilename'
+          placeholder='Profile name'
           control={control}
           rules={{
             required: "Name of profile is required",
@@ -49,73 +42,88 @@ export default function CreateProfileScreen({
             maxLength: { value: 50, message: "Cant be more than 50 letters" },
           }}
         ></CustomInput>
-        {/* Detta är en riktig ful lösning för att toggla rätt inputfält. vill egentligen dölja det ena på något snyggare sätt */}
-        {input ? (
-          <View>
-            <Button style={styles.button} onPress={toggleInput}>
-              <Text>Got a household code already?</Text>
-            </Button>
-            <CustomInput
-              style={styles.input}
-              name="householdname"
-              placeholder="Name of Household"
-              control={control}
-              rules={{
-                required: "Name of household is required",
-                maxLength: {
-                  value: 50,
-                  message: "Cant be more than 50 letters",
-                },
-              }}
-            ></CustomInput>
-            <Button
-              style={styles.button}
-              onPress={handleSubmit(onCreateHouseholdPressed)}
-            >
-              Create Household
-            </Button>
+
+        <View style={styles.separator}></View>
+
+        <View style={{ marginTop: "15%" }}>
+          <Button style={styles.button} onPress={toggleInput}>
+            <Text>
+              {joinAsUser
+                ? "Got a household code already?"
+                : "Dont have a household yet? Create one"}
+            </Text>
+          </Button>
+          <View style={{ marginTop: "5%" }}>
+            {joinAsUser ? (
+              <CustomInput
+                style={styles.input}
+                name='householdname'
+                placeholder='Name of Household'
+                control={control}
+                rules={{
+                  required: "Name of household is required",
+                  maxLength: {
+                    value: 50,
+                    message: "Cant be more than 50 letters",
+                  },
+                }}
+              />
+            ) : (
+              <CustomInput
+                style={styles.input}
+                name='householdcode'
+                placeholder='Code to the household you want to join'
+                control={control}
+                rules={{
+                  required: "Household Code is required",
+                  maxLength: {
+                    value: 10,
+                    message: "Cant be more than 50 letters",
+                  },
+                }}
+              />
+            )}
           </View>
-        ) : (
-          <View>
-            <Button style={styles.button} onPress={toggleInput}>
-              <Text>Dont have a household yet? Create one</Text>
-            </Button>
-            <CustomInput
-              style={styles.input}
-              name="householdcode"
-              placeholder="Code to the household you want to join"
-              control={control}
-              rules={{
-                required: "Household Code is required",
-                maxLength: {
-                  value: 10,
-                  message: "Cant be more than 50 letters",
-                },
-              }}
-            ></CustomInput>
-            <Button
-              style={styles.button}
-              onPress={handleSubmit(onJoinHouseholdPressed)}
-            >
-              Join household
-            </Button>
-          </View>
-        )}
+        </View>
+        <Button
+          style={styles.joinButton}
+          onPress={
+            joinAsUser
+              ? handleSubmit(onCreateHouseholdPressed)
+              : handleSubmit(onJoinHouseholdPressed)
+          }
+        >
+          Create profile and {joinAsUser ? "Household" : "join household"}
+        </Button>
       </View>
     </ScrollView>
   );
 }
+
 const styles = StyleSheet.create({
+  scrollContainer: {
+    height: "100%",
+  },
   container: {
     alignItems: "center",
-    backgroundColor: "green",
-    padding: 20,
+    paddingTop: "30%",
+    paddingHorizontal: 20,
+    height: "100%",
   },
-  title: {
-    backgroundColor: "blue",
+  separator: {
+    marginTop: "15%",
+    height: 8,
+    width: "80%",
+    backgroundColor: "#5553",
+    borderRadius: 50,
   },
-  button: { backgroundColor: "hotpink" },
-
-  input: { backgroundColor: "brown" },
-  dropDownPicker: { backgroundColor: "yellow" },
+  button: { backgroundColor: "#ccc" },
+  joinButton: {
+    position: "absolute",
+    bottom: 5,
+    backgroundColor: "#d0f5",
+    width: "95%",
+    paddingVertical: 10,
+  },
+  input: {},
 });
