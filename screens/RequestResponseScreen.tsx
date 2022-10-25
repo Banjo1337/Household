5342abad4f76b3faf8f3f87bab0678c7eef80427
Jimmy Nordin <jimmy.nordin.1979@gmail.com>
2022-10-25 22:11:10 +0200

    allEsLintWarnings

--- screens/RequestResponseScreen.tsx
+++ screens/RequestResponseScreen.tsx
@@ -3,10 +3,13 @@ import { View } from "react-native";
 import { Title } from "react-native-paper";
 import { RootStackParamList } from "../NavContainer";
 
-export default function RequestResponseScreen(Props: NativeStackScreenProps<RootStackParamList, "RequestResponse">) {
-    return (
-        <View>
-            <Title>Hello from RequestResponseScreen</Title>
-        </View>
-    )
+export default function RequestResponseScreen(
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  Props: NativeStackScreenProps<RootStackParamList, "RequestResponse">,
+) {
+  return (
+    <View>
+      <Title>Hello from RequestResponseScreen</Title>
+    </View>
+  );
 }
